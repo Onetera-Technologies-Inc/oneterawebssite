3f42a4e3f4b301c18fabfe300530e4e8690d15c4
fullstack <rsdesign95@gmail.com>
2024-10-07 21:06:16 +0530

    homedesigns

--- slices/Homebanner/index.tsx
+++ slices/Homebanner/index.tsx
@@ -9,6 +9,7 @@ export type HomebannerProps = SliceComponentProps<Content.HomebannerSlice>;
 /**
  * Component for "Homebanner" Slices.
  */
+
 const Homebanner = ({ slice }: HomebannerProps): JSX.Element => {
   return (
     <div className="main-banner-bg">
@@ -17,7 +18,7 @@ const Homebanner = ({ slice }: HomebannerProps): JSX.Element => {
       data-slice-variation={slice.variation} 
       className="es-bounded"
     >
-     <h1>Access trusted<br/> city services in<br/> one conversation</h1>
+     <h1> Access trusted<br/> city services in<br/> one conversation</h1>
     </section>
     </div>
   );
